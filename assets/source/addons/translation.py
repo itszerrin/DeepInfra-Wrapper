02e530bd404ce971712c0f9225d5701c8ddb1292
4sh <80625238+Recentaly@users.noreply.github.com>
2024-05-28 00:16:51 +0200

    Update translation.py

--- assets/source/addons/translation.py
+++ assets/source/addons/translation.py
@@ -5,7 +5,7 @@ This addon is used to make OpenAI model names point to one of these models. The 
 map = [
     {"gpt-3.5-turbo-0125": "codellama/CodeLlama-34b-Instruct-hf"},
     {"gpt-4-0613": "jondurbin/airoboros-l2-70b-gpt4-1.4.1"},
-    {"gpt-4": "mistralai/Mistral-7B-Instruct-v0.1"},
+    {"gpt-4": "mistralai/Mistral-7B-Instruct-v0.2"},
     {"gpt-4-0125-preview": "mistralai/Mixtral-8x7B-Instruct-v0.1"},
     {"gpt-4-1106-preview": "cognitivecomputations/dolphin-2.6-mixtral-8x7b"},
     {"gpt-4-0314": "lizpreciatior/lzlv_70b_fp16_hf"},
