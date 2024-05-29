b76a204f28e80231196e296efd0d43e703669647
4sh <80625238+Recentaly@users.noreply.github.com>
2024-05-29 21:48:37 +0200

    Update translation.py

--- assets/source/addons/translation.py
+++ assets/source/addons/translation.py
@@ -2,19 +2,16 @@
 This addon is used to make OpenAI model names point to one of these models. The names are semi-random picked.
 """
 
-map = [
-    {"gpt-3.5-turbo-0125": "codellama/CodeLlama-34b-Instruct-hf"},
-    {"gpt-4-0613": "jondurbin/airoboros-l2-70b-gpt4-1.4.1"},
-    {"gpt-4": "mistralai/Mistral-7B-Instruct-v0.2"},
+model_map = [
+    {"gpt-4-0613": ""},
+    {"davinci-002": ""},
+    {"gpt-4": ""},
+    {"gpt-4o": "mistralai/Mistral-7B-Instruct-v0.2"},
     {"gpt-4-0125-preview": "mistralai/Mixtral-8x7B-Instruct-v0.1"},
-    {"gpt-4-1106-preview": "cognitivecomputations/dolphin-2.6-mixtral-8x7b"},
-    {"gpt-4-0314": "lizpreciatior/lzlv_70b_fp16_hf"},
-    {"gpt-3.5-turbo-0613": "deepinfra/airoboros-70b"},
-    {"davinci-002": "HuggingFaceH4/zephyr-orpo-141b-A35b-v0.1"},
-    {"gpt-4o": "microsoft/WizardLM-2-8x22B"},
     {"gpt-4-turbo-preview": "mistralai/Mixtral-8x22B-Instruct-v0.1"},
-    {"gpt-3.5-turbo-instruct-0914": "meta-llama/Meta-Llama-3-70B-Instruct"},
+    {"gpt-4-1106-preview": "cognitivecomputations/dolphin-2.6-mixtral-8x7b"},
     {"gpt-3.5-turbo-16k": "meta-llama/Meta-Llama-3-8B-Instruct"},
+    {"gpt-3.5-turbo-instruct-0914": "meta-llama/Meta-Llama-3-70B-Instruct"},
 ]
 
 def translate(model: str) -> str:
@@ -23,7 +20,7 @@ def translate(model: str) -> str:
     Translate model names to the actual model names
     """
 
-    for i in map:
+    for i in model_map:
         if model in i:
             return i[model]
 
