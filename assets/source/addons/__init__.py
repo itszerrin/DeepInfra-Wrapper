2af73d606bca1e562db3c114b6fbdffe695c5557
4sh <80625238+Recentaly@users.noreply.github.com>
2024-05-29 22:26:00 +0200

    Update __init__.py

--- assets/source/addons/__init__.py
+++ assets/source/addons/__init__.py
@@ -1,11 +1,12 @@
 """
 All addons should be imported here
 """
-from .translation import translate
+from .translation import translate, message_translation
 from .expose import create_cloudflare_tunnel
 
 # here the addons are exported
 __all__ = [
     'translate',
+    'message_translation',
     'create_cloudflare_tunnel'
 ]
